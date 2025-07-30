6631f3d09371f6070b20b7a2f334e47c185c4049
DanielASBatista <daniel_alves.santos@hotmail.com>
2025-07-29 22:23:12 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -8,16 +8,12 @@ namespace HelloWorld
         {
             Console.WriteLine("Digite o seu nome;");
             string nome = Console.ReadLine();
-
             Console.WriteLine($"Seu nome tem {nome.Length} caracteres");
-
             Console.WriteLine("Digite a data do seu nascimento:");
             DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
-
             int  qtdDiasVividos = DateTime.Now.Subtract(dtNascimento).Days;
             Console.WriteLine("Os dias vividos são: "+qtdDiasVividos);
             Console.WriteLine("Os dias vividos são: "+qtdDiasVividos/360);
-
             Console.ReadKey();
         }
     }

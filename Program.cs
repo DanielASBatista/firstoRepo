c7dea6c73866eacbc8f610dbfbdd5f80443f68c3
DanielASBatista <daniel_alves.santos@hotmail.com>
2025-07-29 22:12:26 -0300

    Aula 01

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome;");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Seu nome tem {nome.Length} caracteres");
+
+            Console.WriteLine("Digite a data do seu nascimento:");
+            DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
+
+            int  qtdDiasVividos = DateTime.Now.Subtract(dtNascimento).Days;
+            Console.WriteLine("Os dias vividos são: "+qtdDiasVividos);
+            Console.WriteLine("Os dias vividos são: "+qtdDiasVividos/360);
+
+            Console.ReadKey();
+        }
+    }
+}
